d6dc586e97aaddbd66bf4e49caa96babf372c99a
github username <github email address>
2021-06-24 18:28:17 -0500

    converted methods to class methods and unified them under a main class called main. May change the name from main to somethinge else

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,165 +5,170 @@ require 'net/http'
 require 'json'
 # tool for formatting data retreived from API in Javascript Object Notation
 
+class Main
+  # Initialize data structures to store book data
+  @@readingList = []
+  @@searchResults = nil
 
-# Initialize data structures to store book data
-$readingList = []
-$searchResults = nil
-
-# User session is active as long as value of $active == true
-# Will use this variable to stop application
-$active = true
+  # User session is active as long as value of @@active == true
+  # Will use this variable to stop application
+  @@active = true
 
 
-def sayGoodbye
-  system("clear")
-  puts "Goodbye and thank you for using book search!"
-  puts "-----------------------End-Session---------------------------------------"
+  def self.sayGoodbye
+    system("clear")
+    puts "Goodbye and thank you for using book search!"
+    puts "-----------------------End-Session---------------------------------------"
 
-end
+  end
 
-def promptSearch
-  # prompts user for search query, stores in a variable
-  puts "Please enter the name of an author or book"
-  query = gets.chomp
-  # base url for api call concatenated with user input/search query
-  uri = URI('https://www.googleapis.com/books/v1/volumes?key=AIzaSyBBt_puiZZJw7Q8Fl7jtupk6BvGGpKYMEI&maxResults=5&q=' + query)
-  $searchResults = JSON.parse(Net::HTTP.get(uri))["items"]
+  def self.promptSearch
+    # prompts user for search query, stores in a variable
+    puts "Please enter the name of an author or book"
+    query = gets.chomp
+    # base url for api call concatenated with user input/search query
+    uri = URI('https://www.googleapis.com/books/v1/volumes?key=AIzaSyBBt_puiZZJw7Q8Fl7jtupk6BvGGpKYMEI&maxResults=5&q=' + query)
+    @@searchResults = JSON.parse(Net::HTTP.get(uri))["items"]
 
-end
+  end
 
-def clearTerminal
-  # clears the terminal to promote better readability
-  system("clear")
-end
+  def self.clearTerminal
+    # clears the terminal to promote better readability
+    system("clear")
+  end
 
-def displayInstructions
-  # Displays navigation instructions
-  puts "_____________________________NAVIGATION______________________________"
-  puts "** To add to reading list, enter a number 1-5 that corresponds to your desired book"
-  puts "** To start a new search, enter 6"
-  puts "** To view reading list, enter 7"
-  puts "** To exit, enter 8"
-  puts "_____________________________________________________________________"
-end
-
-def displayBookData(bookDataArray)
-  # Displays the book data, used for displaying search results and reading list data
-  bookDataArray.each_with_index do |book, idx|
+  def self.displayInstructions
+    # Displays navigation instructions
+    puts "_____________________________NAVIGATION______________________________"
+    puts "** To add to reading list, enter a number 1-5 that corresponds to your desired book"
+    puts "** To start a new search, enter 6"
+    puts "** To view reading list, enter 7"
+    puts "** To exit, enter 8"
     puts "_____________________________________________________________________"
-    puts (idx + 1).to_s + ": "
-    puts formatBookData(book)
-
   end
-end
 
-def formatBookData(book)
-  # Displays the Author, Title and Publisher respectively
-  puts "Author(s): #{book["volumeInfo"]["authors"]}"
-  puts "Title: #{book["volumeInfo"]["title"]}"
-  puts "Publisher: #{book["volumeInfo"]["publisher"]}"
-end
+  def self.displayBookData(bookDataArray)
+    # Displays the book data, used for displaying search results and reading list data
+    bookDataArray.each_with_index do |book, idx|
+      puts "_____________________________________________________________________"
+      puts (idx + 1).to_s + ": "
+      puts formatBookData(book)
 
-def addToReadingList(selectedBook)
-  binding.pry
-  $readingList.append($searchResults[selectedBook.to_i])
-  binding.pry
-  clearTerminal()
-  puts "#{$searchResults[selectedBook.to_i]["volumeInfo"]["title"]} has been added to reading list!"
-  displayBookData($searchResults)
-  binding.pry
-  handleUserInput()
-
-end
-
-def handleUserInput
-  # displays instructions for how to navigate system
-  displayInstructions()
-
-  # collects user input to execute instructions
-  input = gets.chomp
-
-# This case handles options outlined in the instructions
-  case input
-    when "1"
-      # saves the first book in the searchResults array to the readingList array...
-      addToReadingList(input)
-
-    when "2"
-      addToReadingList(input)
-
-    when "3"
-      addToReadingList(input)
-
-    when "4"
-      addToReadingList(input)
-
-    when "5"
-      # binding.pry
-      addToReadingList(input)
-    when "6"
-      clearTerminal()
-      # Prompts the user for another search query..
-      run()
-    when "7"
-      # Displays readingList
-      clearTerminal()
-      if $readingList.length == 0
-        # If there are no books in reading list, display the following
-        puts "There are no books in the Reading List!"
-        displayBookData($searchResults)
-        handleUserInput()
-      else
-        # otherwise, displays readingList
-        clearTerminal()
-        puts "_______________________BEGIN_READING_LIST_________________________"
-        $readingList.each_with_index do |book, idx|
-          puts idx.to_s + ": "
-          puts formatBookData(book)
-          puts "------------------------------------------------------------------"
-
-        end
-        puts "_______________________END_READING_LIST___________________________"
-        displayBookData($searchResults)
-        handleUserInput()
-
-      end
-    when "8"
-      # Exits the application
-      clearTerminal()
-      sayGoodbye()
-      $active = false
-      run()
-    else
-      # If invalid response submitted, prompts user for a valid response
-      puts "Please enter a valid response"
-      puts "Press any number to return to the search results"
-      handleUserInput()
     end
-
-end
-
-
-
-def run
-  if $searchResults == nil
-    puts "---------------------Begin-Session---------------------------------------"
-    # puts "Please enter the name of an author or book"
   end
 
-  while $active
-    promptSearch()
+  def self.formatBookData(book)
+    # Displays the Author, Title and Publisher respectively
+    puts "Author(s): #{book["volumeInfo"]["authors"]}"
+    puts "Title: #{book["volumeInfo"]["title"]}"
+    puts "Publisher: #{book["volumeInfo"]["publisher"]}"
+  end
 
-
-    displayBookData($searchResults)
-
-    # display navigation instructions for the interface..
+  def self.addToReadingList(selectedBook)
+    binding.pry
+    @@readingList.append(@@searchResults[selectedBook.to_i])
+    binding.pry
+    clearTerminal()
+    puts "#{@@searchResults[selectedBook.to_i]["volumeInfo"]["title"]} has been added to reading list!"
+    displayBookData(@@searchResults)
+    binding.pry
     handleUserInput()
 
   end
+
+  def self.handleUserInput
+    # displays instructions for how to navigate system
+    displayInstructions()
+
+    # collects user input to execute instructions
+    input = gets.chomp
+
+  # This case handles options outlined in the instructions
+    case input
+      when "1"
+        # saves the first book in the searchResults array to the readingList array...
+        addToReadingList(input)
+
+      when "2"
+        addToReadingList(input)
+
+      when "3"
+        addToReadingList(input)
+
+      when "4"
+        addToReadingList(input)
+
+      when "5"
+        # binding.pry
+        addToReadingList(input)
+      when "6"
+        clearTerminal()
+        # Prompts the user for another search query..
+        run()
+      when "7"
+        # Displays readingList
+        clearTerminal()
+        if @@readingList.length == 0
+          # If there are no books in reading list, display the following
+          puts "There are no books in the Reading List!"
+          displayBookData(@@searchResults)
+          handleUserInput()
+        else
+          # otherwise, displays readingList
+          clearTerminal()
+          puts "_______________________BEGIN_READING_LIST_________________________"
+          @@readingList.each_with_index do |book, idx|
+            puts idx.to_s + ": "
+            puts formatBookData(book)
+            puts "------------------------------------------------------------------"
+
+          end
+          puts "_______________________END_READING_LIST___________________________"
+          displayBookData(@@searchResults)
+          handleUserInput()
+
+        end
+      when "8"
+        # Exits the application
+        clearTerminal()
+        sayGoodbye()
+        @@active = false
+        run()
+      else
+        # If invalid response submitted, prompts user for a valid response
+        puts "Please enter a valid response"
+        puts "Press any number to return to the search results"
+        handleUserInput()
+      end
+
+  end
+
+
+
+  def self.run
+    if @@searchResults == nil
+      puts "---------------------Begin-Session---------------------------------------"
+      # puts "Please enter the name of an author or book"
+    end
+
+    while @@active
+      promptSearch()
+
+
+      displayBookData(@@searchResults)
+
+      # display navigation instructions for the interface..
+      handleUserInput()
+
+    end
+  end
+
+  run()
+
 end
 
-run()
+mainInstance = Main.new
+mainInstance.run
 
 
 
